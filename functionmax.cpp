66d1797d3ff2d35a371d227f2a2a9e38720dc222
markgicharu <markgicharu@hotmail.com>
2016-03-15 18:11:06 +0300

    Update functionmax.cpp

--- functionmax.cpp
+++ functionmax.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+//function prototype
+int max (int a, int b)
 
+//main function
+int main ()
+  {
+    //variable declaration
+    int m,n;
+    //prompt user for 2 integers
+    cout<<"kindly enter 2 integers"<<endl;
+    cout<<"1st integer = ";
+    cin>>m;
+    cout<<"\n 2nd integer = ";
+    cin>>n;
+  }
