caba248e4c53bedcb33a15105ffe9d140c3f6285
markgicharu <markgicharu@hotmail.com>
2016-03-15 19:01:37 +0300

    Update menufunction.cpp

--- menufunction.cpp
+++ menufunction.cpp
@@ -4,5 +4,12 @@ using namespace std;
 void displays()
 {
   cout<<"\t \t MENU \t\t"<<endl;
+  cout<<"\t 1. \t \t ADD"<<endl;
+  
 }
+  //CALCULATOR FUUNCTIONS
+  int addition (int a, int b)
+  {
+    
+  }
   
