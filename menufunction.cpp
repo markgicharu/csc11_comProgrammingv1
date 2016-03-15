2c85eb41153ccd05f5bd9a81f9fd7ab793ee4627
markgicharu <markgicharu@hotmail.com>
2016-03-15 19:09:58 +0300

    Update menufunction.cpp

--- menufunction.cpp
+++ menufunction.cpp
@@ -8,13 +8,30 @@ void displays()
   
 }
   //CALCULATOR FUUNCTIONS
+  //int addition
   int addition (int a, int b)
   {
     return a+b;
   }
+  //int subtraction
   int subtraction (int m, int n)
   {
     return m-n;
+  }
+  //int multiplication
+  int multiplication (int m, int n)
+  {
+    return m*n;
+  }
+  //int division
+  int division (int m, int n)
+  {
+    return m/n;
+  }
+  //int modulus
+  int modulus (int m, int n)
+  {
+    return m%n;
   }
 //MAIN FUNCTION
 
